1088aa042df160433a352e1248661a192fb3ab3e
James Yeap <jamesyeap.ys@gmail.com>
2026-01-09 13:30:08 +0800

    fix: add user input

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,6 +1,6 @@
+use crate::cards::deck::{Card, Deck};
 use std::io;
 use std::io::Write;
-use crate::cards::deck::{Deck, Card};
 
 pub struct Game {
     game_state: GameState,
@@ -9,12 +9,14 @@ pub struct Game {
 impl Game {
     pub fn new() -> Self {
         Self {
-            game_state: GameState::new()
+            game_state: GameState::new(),
         }
     }
 
     /// returns the score at the end of the game
     pub fn start_game(&mut self) -> io::Result<i16> {
+        self.clear_screen();
+
         loop {
             let hand = self.game_state.draw_cards(4);
 
@@ -24,16 +26,68 @@ impl Game {
                 break;
             }
 
-            print!("Type something and press Enter: ");
-            std::io::stdout().flush()?; // ensure prompt is shown
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input)?; // blocks until newline
-            let input = input.trim_end();
-            println!("You typed: {:?}", input);
+            let key = match self.read_user_input() {
+                Ok(input) => input,
+                Err(error) => {
+                    println!("Error: {:?}", error);
+                    continue; // continue to read user input until a valid input is received
+                }
+            };
+
+            // do something with the input
+            match key {
+                // TODO: fetch the actual score from the GameState, and return it
+                0 => {
+                    println!("Exiting the game...");
+                    return Ok(-1); // return the user score
+                }
+
+                // TODO: do something with the chosen card
+                1..=4 => println!("You chose card: {key}\n"),
+
+                // not possible to reach here, as we are already checking that
+                // the user input is between 1 to 4 in read_user_input()
+                _ => println!("Invalid key!"),
+            }
         }
 
-        // TODO: fetch the score from the GameState, and return it
         Ok(-1)
+    }
+
+    fn read_user_input(&self) -> io::Result<u8> {
+        // show prompt to user
+        println!("Enter the card number [1-4] to select it - to quit the game, enter 0:");
+
+        // block for user input, until user hits enter
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        // parse the user input
+        let result = input
+            .trim()
+            .parse::<u8>()
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "is not a valid u8"));
+
+        if let Ok(key) = result
+            && key > 4
+        {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "input can only be 0, 1, 2, 3 or 4.",
+            ));
+        }
+
+        self.clear_screen();
+
+        result
+    }
+
+    fn clear_screen(&self) {
+        // TODO: use crossterm to clear the screen instead of this, as this might not work on Windows
+
+        // ANSI: clear screen and move cursor to 1;1
+        print!("\x1B[2J\x1B[1;1H");
+        io::stdout().flush().ok();
     }
 }
 
@@ -44,7 +98,7 @@ struct GameState {
 
 impl GameState {
     fn new() -> Self {
-        GameState{
+        GameState {
             deck: Deck::default(),
             life: 20,
         }
@@ -63,8 +117,8 @@ impl GameState {
     }
 
     fn put_back_cards(&mut self, cards: Vec<Card>) {
-       for card in cards {
-           self.deck.insert_card(card);
-       }
+        for card in cards {
+            self.deck.insert_card(card);
+        }
     }
 }
